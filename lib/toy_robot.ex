033e6fc70576f31e6a09abb493c34df7f4137bb7
Your Name <you@example.com>
2021-11-12 23:40:13 +0530

    Co-authored-by: Alwin Joseph <LolaDevMan@users.noreply.github.com>

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -51,9 +51,7 @@ defmodule ToyRobot do
   Provide START position to the robot as given location of (x, y, facing) and place it.
   """
   def start(x, y, facing) do
-    ###########################
-    ## complete this funcion ##
-    ###########################
+    {:ok, %ToyRobot.Position{x: x, y: y, facing: facing}}
   end
 
   def stop(_robot, goal_x, goal_y, _cli_proc_name) when goal_x < 1 or goal_y < :a or goal_x > @table_top_x or goal_y > @table_top_y do
@@ -65,9 +63,43 @@ defmodule ToyRobot do
   Passing the CLI Server process name that will be used to send robot's current status after each action is taken.
   """
   def stop(robot, goal_x, goal_y, cli_proc_name) do
-    ###########################
-    ## complete this funcion ##
-    ###########################
+    %ToyRobot.Position{x: x, y: y, facing: facing} = robot
+    IO.puts("#{x} #{y} #{facing}")
+
+    defmodule Loop do
+      @robot_map_y_atom_to_num %{:a => 1, :b => 2, :c => 3, :d => 4, :e => 5}
+
+      def move_up(y,goal_y,robot,_move) when y==goal_y do
+        robot
+      end
+      def move_up(y,goal_y,robot,move) do
+        robot=move.(robot)
+        y=Enum.find(@robot_map_y_atom_to_num, fn {_, val} -> val == Map.get(@robot_map_y_atom_to_num, y) + 1 end) |>elem(0)
+        IO.puts("#{y}")
+        move_up(y,goal_y,robot,move)
+      end
+    end
+
+    robot=Loop.move_up(y,goal_y,robot,&ToyRobot.move/1)
+    flag=0
+    cond do
+      goal_x>x ->
+        flag=1
+        robot=right(robot)
+      goal_x<x ->
+        robot=left(robot)
+      true ->
+        IO.puts("Reached!")
+    end
+    IO.puts("#{flag}")
+
+    # Enum.each(x..goal_x,fn(number)->robot=move(robot)end)
+
+    #IO.puts("#{robot}")
+
+    %ToyRobot.Position{facing: facing, x: x, y: y} = robot
+    IO.puts("#{x} #{y} #{facing}")
+    {:ok,robot}
   end
 
   @doc """
